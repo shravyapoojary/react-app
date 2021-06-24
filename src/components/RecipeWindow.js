0b095a6318a2da882c89260f2df3f51f3a35c058
shravyapoojary <shravyajpoojary@gmail.com>
2021-06-24 17:38:36 +0530

    Update RecipeWindow.js

--- src/components/RecipeWindow.js
+++ src/components/RecipeWindow.js
@@ -6,7 +6,7 @@ const RecipeWindow = ({recipes}) => {
         <div className="RecipeWindow">
           
              {recipes.map(recipe=>(
-                 <div className="recipe" key={`$recipe.recipe.label--${uuidv4()}`} onClick={window.open(recipe.recipe.url)}>
+                 <div className="recipe" key={`$recipe.recipe.label--${uuidv4()}`} onClick={()=>{window.open(recipe.recipe.url)}}>
                      <h2>{recipe.recipe.label}</h2>
                      <img src={recipe.recipe.image} alt={recipe.recipe.label}/>
                  </div>
